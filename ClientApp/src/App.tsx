1a0faa673cb6a1324ae43ffd141e8515798e0f76
Fernando Aguilar <sparkmanperspective@gmail.com>
2024-04-16 21:21:21 -0400

    Main page layout partial completion.

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -5,8 +5,8 @@ export function App() {
     <body>
       <section className="hero">
         <div className="hero-head">
-          <div className="hero-body">
-            <h1 className="is-size-2 has-text-success">GeeksforGeeks</h1>
+          <div className="hero-body has-background-warning">
+            <h1 className="is-size-2 has-text-link">GeeksforGeeks</h1>
 
             <b className="is-size-4">Bulma Container overview</b>
 
@@ -20,7 +20,7 @@ export function App() {
               </div>
             </div>
           </div>
-          <div className="hero-foot">Footer!!!</div>
+          <div className="hero-foot">Footer of the hero section!!!</div>
         </div>
       </section>
 
@@ -30,7 +30,8 @@ export function App() {
           <nav className="navbar">
             <div className="container">
               <div className="navbar-brand">
-                <a className="navbar-item">
+                <a className="navbar-item has-text-warning">
+                  Made with
                   <img
                     src="https://bulma.io/assets/images/bulma-type-white.png"
                     alt="Logo"
@@ -63,9 +64,9 @@ export function App() {
         {/* //end of navbar Top part */}
 
         {/* Hero content: will be in the middle */}
-        <div className="hero-body">
+        <div className="hero-body mb-0 py-5">
           <div className="container has-text-centered">
-            <p className="title">Title</p>
+            <p className="title is-size-3">Please Join!</p>
             <p className="subtitle">
               Subtitle Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Earum maiores possimus quia recusandae.
@@ -73,12 +74,71 @@ export function App() {
           </div>
         </div>
 
+        {/* //Middle section */}
+        <div className="column is-half is-offset-one-quarter">
+          <div className="box p-6 px-10-desktop py-12-desktop has-background-warning has-text-centered">
+            <form action="#">
+              <span className="has-text-link has-text-weight-semibold is-size-4">
+                Sign Up To Join
+              </span>
+              <h3 className="title is-4 mt-4 mb-1">Create new account</h3>
+
+              <div className="is-relative mb-6 ">
+                <input
+                  className="input py-6 has-background-link has-text-warning is-size-3"
+                  type="name"
+                  placeholder="John Doe"
+                />
+                <span className="is-absolute is-top-0 is-left-0 -mt-2 ml-3 has-background-warning has-text-grey-dark is-size-7">
+                  Name
+                </span>
+              </div>
+
+              <div className="is-relative mb-6 ">
+                <input
+                  className="input py-6 has-background-link has-text-warning is-size-3"
+                  type="email"
+                  placeholder="e.g hello@xyz.com"
+                />
+                <span className="is-absolute is-top-0 is-left-0 -mt-2 ml-3 has-background-warning has-text-grey-dark is-size-7">
+                  Your email address
+                </span>
+              </div>
+
+              <div className="is-relative mb-6">
+                <input
+                  className="input py-6 has-background-link has-text-warning is-size-3"
+                  type="password"
+                  placeholder="********"
+                />
+                <span className="is-absolute is-top-0 is-left-0 -mt-2 ml-3 has-background-warning has-text-grey-dark is-size-7">
+                  Password
+                </span>
+              </div>
+              {/* /////end of middle section */}
+
+              <label className="is-flex mb-10">
+                <input
+                  className="mt-1"
+                  type="checkbox"
+                  name="terms"
+                  value="1"
+                />
+                <span className="is-inline-block has-text-grey-dark">
+                  By signing up, you agree to our{' '}
+                  <a href="#">Terms, Data Policy</a>
+                </span>
+              </label>
+            </form>
+          </div>
+        </div>
+
         {/* Hero footer: will stick at the bottom */}
-        <div className="hero-footer">
+        <div className="hero-footer ">
           <nav className="tabs">
-            <div className="container">
-              <ul>
-                <li className="is-active">
+            <div className="column is-three-fifths is-offset-one-fifth">
+              <ul className="is-center">
+                <li className="is-active is-background-warning">
                   <a>Overview</a>
                 </li>
                 <li>
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # BlockbusterMoviesFinal
+
+DOCUMENTATION AND CATALOGUE.
+
+I began by referencing some of the documentation explain by https://www.geeksforgeeks.org/bulma/?ref=lbp.
+In addition, I found a great website which references components written in Bulma style. Link: https://shuffle.dev
+
+1. April 16.
+
+- Installing bulma directly to the ClientApp front end via "npm install bulma".

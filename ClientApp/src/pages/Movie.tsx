8a9cbb59f51b9bac5a3656f07734ce7a9f79b0d4
Fernando Aguilar <sparkmanperspective@gmail.com>
2024-07-09 18:29:24 -0400

    Updated packages via npm.

--- ClientApp/src/pages/Movie.tsx
+++ ClientApp/src/pages/Movie.tsx
@@ -16,7 +16,7 @@ async function loadOneMovie(id: string | undefined) {
   }
 }
 
-// Represent the default value of our object when there is no data being returned from react query. Null object pattern rather than guard clause.
+// Represent the default value of our object when there is no data being returned from react query. Null object pattern rather than guard clause. Handles the case where the object is missing while waiting for the real data to be returned.
 const NullMovie: MovieClassType = {
   id: undefined,
   title: '',
@@ -33,7 +33,7 @@ export function Movie() {
     ['one-movie', id],
     () => loadOneMovie(id)
   )
-  //Guard clause approach
+  //Guard clause approach, as opposed to null object pattern.
   // if (movie === undefined) {
   //   return null
   // }
@@ -306,6 +306,76 @@ export function Movie() {
           <div>
             <h1>Hello world!</h1>
             <h1>{movie.title}</h1>
+          </div>
+          <div className="column is-half is-offset-one-quarter">
+            <div>
+              <a className="navbar-item is-active has-text-primary">Home</a>
+              <Link to="*">Click to go back HOME</Link>
+            </div>
+            <div className="box p-6 px-10-desktop py-12-desktop has-background-warning has-text-centered">
+              <form>
+                <span className="has-text-link has-text-weight-semibold is-size-4">
+                  Viewing detailed data of one movie
+                </span>
+                <h3 className="title is-4 mt-4 mb-1">
+                  Movie title: {movie.title}
+                </h3>
+
+                <div className="is-relative mb-6">
+                  <div className="input py-6 has-background-link has-text-warning is-size-3">
+                    {movie.director}
+                  </div>
+                  <span className="is-absolute is-top-0 is-left-0 -mt-2 ml-3 has-background-warning has-text-grey-dark is-size-7">
+                    Director
+                  </span>
+                </div>
+
+                <div className="is-relative mb-6 ">
+                  <div className="input py-6 has-background-link has-text-warning is-size-3">
+                    {movie.releaseDate}
+                  </div>
+                  <span className="is-absolute is-top-0 is-left-0 -mt-2 ml-3 has-background-warning has-text-grey-dark is-size-7">
+                    Release date
+                  </span>
+                </div>
+
+                <div className="is-relative mb-6 ">
+                  <input
+                    className="input py-6 has-background-link has-text-warning is-size-3"
+                    type="email"
+                    placeholder="e.g hello@xyz.com"
+                  />
+                  <span className="is-absolute is-top-0 is-left-0 -mt-2 ml-3 has-background-warning has-text-grey-dark is-size-7">
+                    Your email address
+                  </span>
+                </div>
+
+                <div className="is-relative mb-6">
+                  <input
+                    className="input py-6 has-background-link has-text-warning is-size-3"
+                    type="password"
+                    placeholder="********"
+                  />
+                  <span className="is-absolute is-top-0 is-left-0 -mt-2 ml-3 has-background-warning has-text-grey-dark is-size-7">
+                    Password
+                  </span>
+                </div>
+                {/* /////end of middle section */}
+
+                <label className="is-flex mb-10">
+                  <input
+                    className="mt-1"
+                    type="checkbox"
+                    name="terms"
+                    value="1"
+                  />
+                  <span className="is-inline-block has-text-grey-dark">
+                    By signing up, you agree to our{' '}
+                    <a href="#">Terms, Data Policy</a>
+                  </span>
+                </label>
+              </form>
+            </div>
           </div>
         </div>
       </section>
